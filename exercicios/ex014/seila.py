b43f0d39e93cf516371c537158d9e7dc6a42c29a
liviafariass <liviafariaas27@gmail.com>
2025-07-25 16:20:00 -0300

    Initial commit

---
+++ exercicios/ex014/seila.py
@@ -0,0 +1,6 @@
+n1 = int(input('insira um numero'))
+n2 = int(input('insira outro numero'))
+s = n1+n2
+#print('a soma entre', n1, 'e', n2, 'é igual a', s)
+print ('a soma entre {} e {} é igual a {}'.format(n1, n2, s))
+
